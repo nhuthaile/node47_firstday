52651ea57f946b834f184680864870fb808a1c67
HAI LE <nhuthai.le@gmail.com>
2024-10-18 14:15:14 +0700

    express buoi 1

---
+++ db.js
@@ -0,0 +1,16 @@
+// Tạo ra một object để kết nối với database
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+// Load biến môi trường từ file .env
+dotenv.config();
+
+const connect = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+export default connect;
